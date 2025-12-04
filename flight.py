04f014895d78dac04c05c8a44840f6d3ce2de531
Rutu <kinnalrutu@gmail.com>
2025-12-04 16:22:39 +0530

    Create flight.py

---
+++ flight.py
@@ -0,0 +1,16 @@
+import sys
+if sys.argv==4:
+  flight_no=sys.argv[1]
+  passenger_name=sys.argv[2]
+  source=sys.argv[3]
+  destination=sys.argv[4]
+else:
+  flight_no=1234
+  passenger_name="rutu"
+  source="hubli"
+  destination="bengalore"
+print("SUMMERY")
+print("flight number:",sys.argv[1])
+print("passenger name:",sys.argv[2])
+print("source:",sys.argv[3])
+print("destination:",sys.argv[4])
